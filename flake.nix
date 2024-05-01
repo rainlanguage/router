c115358d4c9de361991c3ab20ef2cf72ac59874b
rouzwelt <rouzweltt@gmail.com>
2024-05-01 20:16:21 +0000

    update

--- flake.nix
+++ flake.nix
@@ -1,25 +1,42 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    rainix.url = "github:rainprotocol/rainix";
+    # rainix.url = "github:rainprotocol/rainix";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, flake-utils, rainix }:
+  outputs = { self, flake-utils, rust-overlay }:
 
   flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = rainix.pkgs.${system};
+      overlays =[ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust-version = "1.76.0";
+        rust-toolchain = pkgs.rust-bin.stable.${rust-version}.default.override (previous: {
+          targets = previous.targets ++ [ "wasm32-unknown-unknown" ];
+        });
     in {
       # For `nix develop`:
       devShell = pkgs.mkShell {
         nativeBuildInputs = [
-          rainix.rust-build-inputs.${system}
-          rainix.packages.${system}.rainix-rs-test
-          rainix.packages.${system}.rainix-rs-artifacts
-          rainix.packages.${system}.rainix-rs-prelude
-          rainix.packages.${system}.rainix-rs-static
+          rust-toolchain
+          pkgs.cargo-release
+          pkgs.gmp
+          pkgs.openssl
+          pkgs.libusb
+          pkgs.pkg-config
+          pkgs.wasm-bindgen-cli
+          pkgs.gettext
+          pkgs.libiconv
+          pkgs.cargo-flamegraph
+          # rainix.rust-build-inputs.${system}
+          # rainix.packages.${system}.rainix-rs-test
+          # rainix.packages.${system}.rainix-rs-artifacts
+          # rainix.packages.${system}.rainix-rs-prelude
+          # rainix.packages.${system}.rainix-rs-static
         ];
-        shellHook = ''sudo apt install libssl-dev'';
       };
     }
   );
