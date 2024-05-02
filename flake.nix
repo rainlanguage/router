8dcc9e206432c40df38876232e268c9d0b6fc8c4
thedavidmeister <thedavidmeister@gmail.com>
2024-05-02 13:40:40 +0400

    debug openssl

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     in {
       # For `nix develop`:
       devShell = pkgs.mkShell {
-        nativeBuildInputs = [
+        buildInputs = [
           # pkgs.openssl
           rainix.rust-build-inputs.${system}
           rainix.packages.${system}.rainix-rs-test
