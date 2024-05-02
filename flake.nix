9236d25726cb0a0eb412d225629cbd677fa63939
thedavidmeister <thedavidmeister@gmail.com>
2024-05-02 14:05:46 +0400

    lint shell

--- flake.nix
+++ flake.nix
@@ -7,21 +7,7 @@
   outputs = { self, flake-utils, rainix }:
 
   flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = rainix.pkgs.${system};
-    in {
-      # For `nix develop`:
-      # devShell = pkgs.mkShell {
-      #   inputs
-      #   # inputsFrom = [
-      #   #   # pkgs.openssl
-      #   #   rainix.rust-build-inputs.${system}
-      #   #   rainix.packages.${system}.rainix-rs-test
-      #   #   rainix.packages.${system}.rainix-rs-artifacts
-      #   #   rainix.packages.${system}.rainix-rs-prelude
-      #   #   rainix.packages.${system}.rainix-rs-static
-      #   # ];
-      # };
+    {
       devShells.default = rainix.devShells.${system}.default;
     }
   );
