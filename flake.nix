2d8c2762474156645149d8db0b81c01716d5a96e
thedavidmeister <thedavidmeister@gmail.com>
2024-05-02 13:36:12 +0400

    debug openssl nix

--- flake.nix
+++ flake.nix
@@ -9,13 +9,12 @@
   flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = rainix.pkgs.${system};
-      rust-build-inputs = rainix.rust-build-inputs.${system};
     in {
       # For `nix develop`:
       devShell = pkgs.mkShell {
         nativeBuildInputs = [
-          pkgs.openssl
-          rust-build-inputs
+          # pkgs.openssl
+          rainix.rust-build-inputs.${system}
           rainix.packages.${system}.rainix-rs-test
           rainix.packages.${system}.rainix-rs-artifacts
           rainix.packages.${system}.rainix-rs-prelude
